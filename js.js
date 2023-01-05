ba2acf3ab2252dfa8e81e6e6f749ebc4e6b0b3a1
MartianouriH <108149715+MartianouriH@users.noreply.github.com>
2023-01-05 18:59:58 +0330

    Add files via upload

---
+++ js.js
@@ -0,0 +1,211 @@
+additional = {
+    mercury : `
+        <p>
+            Mercury is the smallest and innermost planet in the Solar System. Its orbital period around the Sun 
+            of 87.97 days is the shortest of all the planets in the Solar System. It is named after the Roman 
+            deity Mercury, the messenger of the gods.
+        </p>
+        <p>
+            Like Venus, Mercury orbits the Sun within Earth's orbit as an inferior planet, and never exceeds 28° 
+            away from the Sun when viewed from Earth. This proximity to the Sun means the planet can only be seen 
+            near the western or eastern horizon during the early evening or early morning. At this time it may appear 
+            as a bright star-like object, but is often far more difficult to observe than Venus. The planet 
+            telescopically displays the complete range of phases, similar to Venus and the Moon, as it moves in 
+            its inner orbit relative to Earth, which reoccurs over the so-called synodic period approximately every 116 days.
+        </p>`,
+    venus: `
+        <p> 
+            Venus is the second planet from the Sun, orbiting it every 224.7 Earth days. It has the longest rotation period 
+            (243 days) of any planet in the Solar System and rotates in the opposite direction to most other planets (meaning 
+            the Sun rises in the west and sets in the east). It does not have any natural satellites. It is named after the 
+            Roman goddess of love and beauty. It is the second-brightest natural object in the night sky after the Moon, 
+            reaching an apparent magnitude of −4.6 – bright enough to cast shadows at night and, rarely, visible to the naked 
+            eye in broad daylight. Orbiting within Earth's orbit, Venus is an inferior planet and never appears to venture 
+            far from the Sun; its maximum angular distance from the Sun (elongation) is 47.8°.
+        </p>`,
+    earth: `
+        <p>
+            Earth is the third planet from the Sun, and the only astronomical object known to harbor life. According to radiometric 
+            dating and other sources of evidence, Earth formed over 4.5 billion years ago. Earth's gravity interacts with other 
+            objects in space, especially the Sun and the Moon, Earth's only natural satellite. Earth orbits around the Sun in 365.26 
+            days, a period known as an Earth year. During this time, Earth rotates about its axis about 366.26 times. Earth's axis 
+            of rotation is tilted with respect to its orbital plane, producing seasons on Earth. The gravitational interaction 
+            between Earth and the Moon causes tides, stabilizes Earth's orientation on its axis, and gradually slows its rotation. 
+            Earth is the densest planet in the Solar System and the largest and most massive of the four terrestrial planets.
+        </p>
+    `,
+    mars: `
+        <p>
+            Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System after Mercury. 
+            In English, Mars carries a name of the Roman god of war, and is often referred to as the "Red Planet" because the
+            iron oxide prevalent on its surface gives it a reddish appearance that is distinctive among the astronomical bodies 
+            visible to the naked eye. Mars is a terrestrial planet with a thin atmosphere, having surface features reminiscent 
+            both of the impact craters of the Moon and the valleys, deserts, and polar ice caps of Earth. The days and seasons 
+            are likewise comparable to those of Earth, because the rotational period as well as the tilt of the rotational axis 
+            relative to the ecliptic plane are very similar. Mars is the site of Olympus Mons, the largest volcano and 
+            second-highest known mountain in the Solar System, and of Valles Marineris, one of the largest canyons in the Solar System. 
+            The smooth Borealis basin in the northern hemisphere covers 40% of the planet and may be a giant impact feature. 
+            Mars has two moons, Phobos and Deimos, which are small and irregularly shaped. These may be captured asteroids, 
+            similar to 5261 Eureka, a Mars trojan. 
+        </p>
+        `,
+    jupiter: 
+        `<p>
+            Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a giant planet with a mass 
+            one-thousandth that of the Sun, but two-and-a-half times that of all the other planets in the Solar System combined. 
+            Jupiter and Saturn are gas giants; the other two giant planets, Uranus and Neptune, are ice giants. Jupiter has been 
+            known to astronomers since antiquity. It is named after the Roman god Jupiter. When viewed from Earth, Jupiter 
+            can reach an apparent magnitude of −2.94, bright enough for its reflected light to cast shadows, and making it on 
+            average the third-brightest natural object in the night sky after the Moon and Venus.
+        </p>
+        `,
+    saturn:
+        `<p>
+            Saturn is the sixth planet from the Sun and the second-largest in the Solar System, after Jupiter. It is a gas 
+            giant with an average radius about nine times that of Earth. It has only one-eighth the average density 
+            of Earth, but with its larger volume Saturn is over 95 times more massive.Saturn is named after the 
+            Roman god of agriculture; its astronomical symbol (♄) represents the god's sickle. Saturn's interior is probably 
+            composed of a core of iron–nickel and rock (silicon and oxygen compounds). This core is surrounded by a deep layer 
+            of metallic hydrogen, an intermediate layer of liquid hydrogen and liquid helium, and finally a gaseous outer layer. 
+            Saturn has a pale yellow hue due to ammonia crystals in its upper atmosphere. Electrical current within the metallic
+             hydrogen layer is thought to give rise to Saturn's planetary magnetic field, which is weaker than Earth's, but has a
+              magnetic moment 580 times that of Earth due to Saturn's larger size. Saturn's magnetic field strength is 
+              around one-twentieth of Jupiter's. The outer atmosphere is generally bland and lacking in contrast, although 
+            long-lived features can appear. 
+        </p>`,
+    neptune:
+        `<p>
+            Neptune is the eighth and farthest known planet from the Sun in the Solar System. In the Solar System, it is the 
+            fourth-largest planet by diameter, the third-most-massive planet, and the densest giant planet. Neptune is 17 times 
+            the mass of Earth, slightly more massive than its near-twin Uranus. Neptune is denser and physically smaller than 
+            Uranus because its greater mass causes more gravitational compression of its atmosphere. Neptune orbits the Sun once 
+            every 164.8 years at an average distance of 30.1 AU (4.5 billion km). It is named after the Roman god of the sea and 
+            has the astronomical symbol ♆, a stylised version of the god Neptune's trident. Neptune is not visible to the unaided 
+            eye and is the only planet in the Solar System found by mathematical prediction rather than by empirical observation. 
+            Unexpected changes in the orbit of Uranus led Alexis Bouvard to deduce that its orbit was subject to gravitational 
+            perturbation by an unknown planet. Neptune was subsequently observed with a telescope on 23 September 1846 by Johann 
+            Galle within a degree of the position predicted by Urbain Le Verrier. Its largest moon, Triton, was discovered shortly 
+            thereafter, though none of the planet's remaining known 13 moons were located telescopically until the 20th century.
+        </p>`,
+    uranus:
+        `<p>
+            Uranus (from the Latin name Ūranus for the Greek god Οὐρανός) is the seventh planet from the Sun. It has the third-largest 
+            planetary radius and fourth-largest planetary mass in the Solar System. Uranus is similar in composition to Neptune, 
+            and both have bulk chemical compositions which differ from that of the larger gas giants Jupiter and Saturn. For this 
+            reason, scientists often classify Uranus and Neptune as "ice giants" to distinguish them from the gas giants. Uranus' 
+            atmosphere is similar to Jupiter's and Saturn's in its primary composition of hydrogen and helium, but it contains 
+            more "ices" such as water, ammonia, and methane, along with traces of other hydrocarbons. It is the coldest planetary 
+            atmosphere in the Solar System, with a minimum temperature of 49 K (−224 °C; −371 °F), and has a complex, layered cloud 
+            structure with water thought to make up the lowest clouds and methane the uppermost layer of clouds. The interior of 
+            Uranus is mainly composed of ices and rock.
+        </p>`
+
+}
+
+window.onload = () => {
+    document.querySelectorAll('.planet-info').forEach(
+        block => block.onclick = (event) => {
+            const planet = event.target.dataset.planet
+
+            document.querySelector('.about').innerHTML = additional[planet]
+            document.querySelector('.additional').classList.add('show')
+            document.querySelector('.close').classList.add('show')
+        }
+    )
+
+    document.querySelector('.close').onclick = () => {
+        document.querySelector('.about').innerHTML = ''
+        document.querySelector('.additional').classList.remove('show') 
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const random = (max) => {
+    return (Math.floor(Math.random() * Math.floor(max)) + 1);
+  }
+  
+  const stars = () => {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    let i = 0;
+    
+    // C O N F I G
+    const sizeLimit = 1; // max star size (in pixels)
+    const count = width / 5; // lower number === more stars
+    const twinkle = false;
+    // T H A T I S A L L
+    
+    while (i < count) {
+      const randomSize = random(sizeLimit);
+      const opacity = random(100);
+      const randomTop = random(height);
+      const randomLeft = random(width);
+      const randomHue = random(360);
+      const glow = `
+        0 0 ${ randomSize * 7 }px hsl(${ randomHue }, 50%, 50%)
+      `;
+      const twinkleTime = (random(10) / 10) + 2;
+      
+      document.body.innerHTML += `
+        <div class="star ${ twinkle === true ? 'twinkle' : '' }"
+          style="
+            opacity: ${ opacity / 100 };
+            height: ${ randomSize }px;
+            width: ${ randomSize }px;
+            top: ${ randomTop }px;
+            left: ${ randomLeft }px;
+            animation-duration: ${ twinkleTime }s;
+            box-shadow:
+              inset 0 0 ${ randomSize / 4 }px hsl(${ randomHue }, 50%, 50%),
+              ${
+                randomSize === sizeLimit && opacity > 50 ?
+                glow : false
+              };
+          "
+        ></div>
+      `;
+      i++;
+    }
+  }
+  
+  stars();
